b182f733eef29f56e5668c3b2ab55ce3a59f24e7
Alexandr Lukyanets <alexlukyanets18@gmail.com>
2020-12-01 17:59:42 +0200

    Finish category

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -48,6 +48,9 @@ class Shop(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
+        if self.kwargs:
+            context['title_category'] = str(Category.objects.get(slug=self.kwargs['slug']))
+
         return context
 
 
